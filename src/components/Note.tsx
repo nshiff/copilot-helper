f79ed7d8b681b39d1e0fe451296c12ebe5c8b7ed
nshiff <nshiff@users.noreply.github.com>
2025-02-11 15:41:19 -0500

    removing unused files

--- src/components/Note.tsx
+++
@@ -1,8 +0,0 @@
-export function Note({ title, content }: { title: string; content: string }) {
-  return (
-    <div className="note">
-      <h3>{title}</h3>
-      <div>{content}</div>
-    </div>
-  );
-}
